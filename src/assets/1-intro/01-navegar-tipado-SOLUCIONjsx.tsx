fbdff8ee3f511dd27c98efc2c77e813fe3e55805
Matias Baldanza <matiasbaldanza@gmail.com>
2025-01-29 13:34:33 -0300

    1-01 como navegar tipado de JSX

---
+++ src/assets/1-intro/01-navegar-tipado-SOLUCIONjsx.tsx
@@ -0,0 +1,10 @@
+export const Componente = () => {
+  return (
+    <div                  // control-click on me to go to the div types
+      aria-posinset={1}   // number | undefined
+      onChange={() => { }} // React.FormEventHandler<HTMLDivElement> | undefined
+
+    // Para tener autocompletado en JSX: ctrl-space
+    />
+  );
+};
---
+++ src/assets/1-intro/01-navegar-tipado-jsx.tsx
@@ -0,0 +1,10 @@
+export const Componente = () => {
+  return (
+    <div                  // control-click on me to go to the div types
+      aria-posinset={ }   // hover para ver la definición.
+      onChange={ } // cómo sé que tipo expera onChange? 
+
+    // Para tener autocompletado en JSX: ctrl-space
+    />
+  );
+};
